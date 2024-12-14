8ae1ff6baf1b6309b393755c15c69071a41fd603
nayanbirla <nayanbirla9893@gmail.com>
2024-12-15 00:46:16 +0530

    code fixes

--- src/test/java/com/experiment/SpringExperimentTest.java
+++ src/test/java/com/experiment/SpringExperimentTest.java
@@ -2,19 +2,23 @@ package com.experiment;
 
 import com.expermient.Main;
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.junit.platform.commons.logging.Logger;
+import org.junit.platform.commons.logging.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.function.Supplier;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest
+@SpringBootTest(classes = Main.class)
 public class SpringExperimentTest {
-
-    public static Logger logger= LoggerFactory.getLogger(Main.class);
+    public static Logger logger = LoggerFactory.getLogger(Main.class);
 
     @Test
-    public void contextLoads(){
-        logger.info("Test case execution...");
-        assertEquals(true,true);
+    public void contextLoads() {
+        String someValue="chalega";
+        Supplier<String> supplier = () -> "Message: " + someValue;
+        logger.debug(supplier);
+        assertEquals(true, true);
     }
 }
